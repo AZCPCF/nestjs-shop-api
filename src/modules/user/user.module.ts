7f25fdc016ae6d36c8a9350984569146a008f04e
Alisan <144158983+AZCPCF@users.noreply.github.com>
2025-12-22 01:07:38 +0330

    feat(auth): implement secure password hashing with bcrypt

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import { PasswordModule } from '../password/password.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/entities/user.entity';
 
 @Module({
+  imports: [PasswordModule, TypeOrmModule.forFeature([User])],
   controllers: [UserController],
   providers: [UserService],
+  exports: [UserService],
 })
 export class UserModule {}
