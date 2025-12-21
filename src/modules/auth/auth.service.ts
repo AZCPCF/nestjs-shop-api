683173379c5b493ad05a2695bc4dfba4fd812170
Alisan <144158983+AZCPCF@users.noreply.github.com>
2025-12-22 02:16:51 +0330

    feat(auth): add register endpoint (POST /auth/register)

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,10 +1,22 @@
 import { Injectable } from '@nestjs/common';
 import { PasswordService } from '../password/password.service';
+import { Repository } from 'typeorm';
+import { User } from 'src/entities/user/user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserService } from '../user/user.service';
 
 @Injectable()
 export class AuthService {
-    constructor(private readonly passwordService:PasswordService){}
-    get(p:string){
-        return this.passwordService.hash(p)
-    }
+  constructor(
+    private readonly passwordService: PasswordService,
+    private readonly userService: UserService,
+  ) {}
+  async register(
+    email: string,
+    password: string,
+    displayName: string,
+  ): Promise<User> {
+    const hashedPassword = await this.passwordService.hash(password);
+    return this.userService.create(email, hashedPassword, displayName);
+  }
 }
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User, UserEnum } from 'src/entities/user/user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    @InjectRepository(User) private readonly userRepo: Repository<User>,
+  ) {}
+  async create(
+    email: string,
+    password: string,
+    displayName: string,
+  ): Promise<User> {
+    const user = this.userRepo.create({
+      email,
+      password,
+      role: UserEnum.USER,
+      displayName,
+    });
+    return await this.userRepo.save(user);
+  }
+}
