b5a196d8f689fa14e9b5d1188b543264e252f1ee
Alisan <144158983+AZCPCF@users.noreply.github.com>
2025-12-28 17:09:38 +0330

    feat(auth): add RolesGuard for USER/ADMIN role-based access

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,12 +1,17 @@
 import { Controller, Get, Req, UseGuards } from '@nestjs/common';
 import { UserService } from './user.service';
 import { AuthGuard } from '@nestjs/passport';
+import { RolesGuard } from 'src/common/guards/roles.guard';
+
+import { Roles } from 'src/common/decorators/roles.decorator';
+import { Role } from 'src/entities/user/user.entity';
 
 @Controller('user')
-@UseGuards(AuthGuard('jwt'))
+@UseGuards(AuthGuard('jwt'),RolesGuard)
 export class UserController {
   constructor(private readonly userService: UserService) {}
   @Get('me')
+  @Roles(Role.USER,Role.ADMIN)
   getMe(@Req() req: Request) {
     return req.user;
   }
